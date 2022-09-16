0346d89699a32a620001d0f088ea0914ef8421cd
Josh Chappelow <jdchappelow@gmail.com>
2022-09-16 13:04:33 +0900

    normalize

--- framework/shopify/products/get-all-products.ts
+++ framework/shopify/products/get-all-products.ts
@@ -1,17 +1,22 @@
 import getAllProductsQuery from "../utils/queries/get-all-products";
 import fetchApi from "../utils/fetch-api";
-import { ProductConnection } from "../schema";
+import { ProductCollectionSortKeys, ProductConnection } from "../schema";
+import { normalizeProduct } from "../utils/normalize";
 
 type ReturnType = {
   products: ProductConnection;
 };
 
 const getAllProducts = async (): Promise<any> => {
-  const { data } = await fetchApi<{ products: ProductConnection }>({
+  const { data } = await fetchApi<ReturnType>({
     query: getAllProductsQuery,
   });
 
-  return data.products;
+  const products =
+    data.products.edges.map(({ node: product }) => normalizeProduct(product)) ??
+    [];
+
+  return products;
 };
 
 export default getAllProducts;
