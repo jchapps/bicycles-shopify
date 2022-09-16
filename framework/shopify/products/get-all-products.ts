06d0e0eb94c03295713e7cb13d6a9aac571a65a7
Josh Chappelow <jdchappelow@gmail.com>
2022-09-16 11:33:22 +0900

    product connection

--- framework/shopify/products/get-all-products.ts
+++ framework/shopify/products/get-all-products.ts
@@ -1,29 +1,17 @@
 import getAllProductsQuery from "../utils/queries/get-all-products";
+import fetchApi from "../utils/fetch-api";
+import { ProductConnection } from "../schema";
 
-type FetchParams = {
-  query: string;
+type ReturnType = {
+  products: ProductConnection;
 };
 
-const fetchApi = async ({ query }: { query: string }) => {
-  const url = "http://localhost:4000/graphql";
-
-  const res = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: getAllProductsQuery,
-    }),
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<{ products: ProductConnection }>({
+    query: getAllProductsQuery,
   });
 
-  const data = await res.json();
-  return { data };
-};
-
-const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi({ query: getAllProductsQuery });
-  return products.data;
+  return data.products;
 };
 
 export default getAllProducts;
