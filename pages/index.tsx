e0418c0062608118b15e7500f9cd1d6e0092c11d
Josh Chappelow <jdchappelow@gmail.com>
2022-12-02 16:51:05 +0900

    feat: marquee component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import getAllproducts from "@framework/product/get-all-products";
 import { getConfig } from "@framework/api/config";
 import { Layout } from "@components/common";
 import { ProductCard } from "@components/product";
-import { Grid, Hero } from "@components/ui";
+import { Grid, Hero, Marquee } from "@components/ui";
 
 export async function getStaticProps() {
   const config = getConfig();
@@ -24,10 +24,18 @@ export default function Home({
     <>
       <Grid>
         {products.slice(0, 3).map((product) => (
-          <ProductCard key={product.id} product={product}/>
+          <ProductCard key={product.id} product={product} />
         ))}
       </Grid>
-      <Hero headline="hello baked beans, heinz boyz" description="wow thats great mate, wow nice rune scimmy"/>
+      <Hero
+        headline="hello baked beans, heinz boyz"
+        description="wow thats great mate, wow nice rune scimmy"
+      />
+      <Marquee>
+      {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Marquee>
     </>
   );
 }
