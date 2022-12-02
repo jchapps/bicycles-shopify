5d5eb55e0a59e853bfc5b65b3f442c760324e73c
Josh Chappelow <jdchappelow@gmail.com>
2022-12-02 15:23:22 +0900

    feat: product card styling

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,33 +1,36 @@
 import { Product } from "@common/types/product";
 import { FC } from "react";
 import Link from "next/link";
-import Image from "next/image"
+import Image from "next/image";
+import styles from "./ProductCard.module.css";
 
 interface Props {
   product: Product;
 }
 
-const placeholderImg = "/product-image-placeholder.svg"
+const placeholderImg = "/product-image-placeholder.svg";
 const ProductCard: FC<Props> = ({ product }) => {
-
   return (
     <Link href={`/products/${product.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={styles.root}>
+        <div className={styles.productBg}></div>
+        <div className={styles.productTag}>
+          <h3 className={styles.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>100</span>
-            {product.images && <Image
-              alt={product.name ?? "Product Name"}
-              src={product.images[0].url ?? placeholderImg}
-              height={540}
-              width={540}
-              quality="85"
-              layout="responsive"
-            />}
-
+          <span className={styles.productPrice}>5 $</span>
         </div>
+        {product.images && (
+          <Image
+            className={styles.productImage}
+            alt={product.name ?? "Product Name"}
+            src={product.images[0].url ?? placeholderImg}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
       </a>
     </Link>
   );
