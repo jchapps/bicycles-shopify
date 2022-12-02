2dd9511bb3629b72bf6d3abb95195b59a5a73b43
Josh Chappelow <jdchappelow@gmail.com>
2022-12-02 18:04:31 +0900

    feat: basic footer

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,14 +1,22 @@
 import React, { ReactNode, FC } from "react";
 import styles from "./Marquee.module.css";
 import Ticker from "react-ticker";
+import classname from 'classnames'
 
 interface Props {
   children: ReactNode[];
+  variant?: "primary" | "secondary"
 }
 
-const Marquee: FC<Props> = ({ children }) => {
+const Marquee: FC<Props> = ({ children, variant="primary" }) => {
+  const rootClassName = classname(
+    styles.root,
+    {
+      [styles.secondary]: variant === "secondary"
+    }
+  )
   return (
-    <div className={styles.root}>
+    <div className={rootClassName}>
       <Ticker offset={100}>
         {() => <div className={styles.container}>{children}</div>}
       </Ticker>
--- pages/index.tsx
+++ pages/index.tsx
@@ -42,6 +42,11 @@ export default function Home({
           <ProductCard variant = "slim" key={product.id} product={product} />
         ))}
       </Grid>
+      <Marquee variant="secondary">
+      {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Marquee>
     </>
   );
 }
