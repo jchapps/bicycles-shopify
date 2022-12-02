4a3ea1aeed7ec0e21bfc3728da9b2c93cc267ec8
Josh Chappelow <jdchappelow@gmail.com>
2022-12-02 14:18:44 +0900

    feat: layout choices

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,17 +1,18 @@
-import {FC, ReactNode } from 'react';
-import styles from "./Grid.module.css"
+import { FC, ReactNode } from "react";
+import styles from "./Grid.module.css";
+import classnames from "classnames";
 
 interface Props {
-  children: ReactNode[]
-  layout?: "A" | "B"
+  children: ReactNode[];
+  layout?: "A" | "B";
 }
 
-const Grid:FC = ({children}) => {
-  return (
-    <div className={styles.root}>
-      {children}
-    </div>
-  );
-}
+const Grid: FC<Props> = ({ children, layout = "A" }) => {
+  const rootClassName = classnames(styles.root, {
+    [styles.layoutA]: layout === "A",
+    [styles.layoutB]: layout === "B",
+  });
+  return <div className={rootClassName}>{children}</div>;
+};
 
 export default Grid;
