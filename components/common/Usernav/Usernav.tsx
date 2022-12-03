2eba7473e220a616c2ad41305d9b41a664a850b6
Josh Chappelow <jdchappelow@gmail.com>
2022-12-03 22:15:29 +0900

    feat: icons for cart and wishlist

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -1,16 +1,17 @@
 import { FC } from "react";
 import styles from "./Usernav.module.css";
 import Link from "next/link";
+import {Bag as Cart, Heart} from '@components/icons'
 
 const Usernav: FC = () => {
   return (
     <div>
       <ul className={styles.list}>
-        <li className={styles.item}>Cart</li>
+        <li className={styles.item}><Cart/></li>
         <li className={styles.item}>
           <Link href="/">
             <a>
-            Wishlist
+            <Heart/>
             </a>
           </Link>
         </li>
