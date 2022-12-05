11cfb3dedfdf9047881fd844584b083650be7604
Josh Chappelow <jdchappelow@gmail.com>
2022-12-05 20:12:09 +0900

    feat: basic product id page

---
+++ pages/products/[slug].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '@components/common';
+
+export default function ProductSlug() {
+  return (
+    <div>
+      hihi
+    </div>
+  );
+}
+
+ProductSlug.Layout = Layout
