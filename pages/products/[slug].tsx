b9de93dab3a0ea28d3c25e006eb1af7523ea4118
Josh Chappelow <jdchappelow@gmail.com>
2022-12-07 16:33:04 +0900

    feat: prduct passed through props

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,10 +1,32 @@
-import React from 'react';
 import { Layout } from '@components/common';
+import { GetStaticPaths, GetStaticPathsContext, GetStaticPropsContext, InferGetServerSidePropsType } from 'next';
 
-export default function ProductSlug() {
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: {slug: "cool-hat"}},
+      { params: {slug: "t-shirt"}},
+      { params: {slug: "lightweight-jacket"}}
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({params}: GetStaticPropsContext<{slug: string}>) => {
+  return {
+    props: {
+      product: {
+        slug: params?.slug
+      }
+    }
+  }
+}
+
+export default function ProductSlug({ product }: InferGetServerSidePropsType<typeof getStaticProps>) {
   return (
     <div>
-      hihi
+      {product.slug}
     </div>
   );
 }
