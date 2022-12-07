af06efd694ea9b34bb6465358080d6c543c431d6
Josh Chappelow <jdchappelow@gmail.com>
2022-12-07 22:29:00 +0900

    feat: active choice state

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,24 +1,34 @@
-import cn from "classnames";
+
+import cn from 'classnames'
 import { FC, useState } from 'react'
-import s from "./ProductView.module.css";
-import { Container, Button } from "@components/ui";
-import Image from "next/image";
-import { Product } from "@common/types/product";
+import s from './ProductView.module.css'
+import { Container, Button } from '@components/ui'
+import Image from "next/image"
+import { Product } from '@common/types/product'
 import { ProductSlider, Swatch } from "@components/product"
 
 interface Props {
-  product: Product;
+  product: Product
+}
+
+type AvailableChoices = "color" | "size" | string
+
+type Choices = {
+  [P in AvailableChoices]: string
 }
 
 const ProductView: FC<Props> = ({ product }) => {
-  const [ choices, setChoices ] = useState({})
+  const [ choices, setChoices ] = useState<Choices>({})
 
+  console.log(choices)
   return (
     <Container>
-      <div className={cn(s.root, "fit", "mb-5")}>
-        <div className={cn(s.productDisplay, "fit")}>
+      <div className={cn(s.root, 'fit', "mb-5")}>
+        <div className={cn(s.productDisplay, 'fit')}>
           <div className={s.nameBox}>
-            <h1 className={s.name}>{product.name}</h1>
+            <h1 className={s.name}>
+              {product.name}
+            </h1>
             <div className={s.price}>
               {product.price.value}
               {` `}
@@ -26,7 +36,7 @@ const ProductView: FC<Props> = ({ product }) => {
             </div>
           </div>
           <ProductSlider>
-            {product.images.map((image) => (
+            { product.images.map(image =>
               <div key={image.url} className={s.imageContainer}>
                 <Image
                   className={s.img}
@@ -37,7 +47,7 @@ const ProductView: FC<Props> = ({ product }) => {
                   quality="85"
                 />
               </div>
-            ))}
+            )}
           </ProductSlider>
         </div>
         <div className={s.sidebar}>
@@ -46,38 +56,42 @@ const ProductView: FC<Props> = ({ product }) => {
               <div key={option.id} className="pb-4">
                 <h2 className="uppercase font-medium">{option.displayName}</h2>
                 <div className="flex flex-row py-4">
-                  { option.values.map(optValue =>
-                    <Swatch
-                      key={`${option.id}-${optValue.label}`}
-                      label={optValue.label}
-                      color={optValue.hexColor}
-                      variant={option.displayName}
-                      onClick={() => {
-                        setChoices({
-                          ...choices,
-                          [option.displayName.toLowerCase()]: optValue.label.toLowerCase()
-                        })
-                      }}
-                    />
+                  { option.values.map(optValue => {
+                    const activeChoice = choices[option.displayName.toLowerCase()]
+                    return (
+                      <Swatch
+                        key={`${option.id}-${optValue.label}`}
+                        label={optValue.label}
+                        color={optValue.hexColor}
+                        variant={option.displayName}
+                        active={optValue.label.toLowerCase() === activeChoice}
+                        onClick={() => {
+                          setChoices({
+                            ...choices,
+                            [option.displayName.toLowerCase()]: optValue.label.toLowerCase()
+                          })
+                        }}
+                      />
+                    )}
                   )}
                 </div>
               </div>
             )}
             <div className="pb-14 break-words w-full max-w-xl text-lg">
-              {product.description}
+              { product.description }
             </div>
           </section>
           <div>
-          <Button
+            <Button
               className={s.button}
               onClick={() => alert("adding to cart")}>
               Add to Cart
-          </Button>
+            </Button>
           </div>
         </div>
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default ProductView;
+export default ProductView
