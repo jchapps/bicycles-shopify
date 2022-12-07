0259bb9148fce494c8bc2e0c4847579e580cdbc8
Josh Chappelow <jdchappelow@gmail.com>
2022-12-07 21:41:18 +0900

    build: keen slider package

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,49 +1,51 @@
-import cn from 'classnames'
-import { FC } from 'react'
-import s from './ProductView.module.css'
-import { Container } from '@components/ui'
-import Image from "next/image"
-import { Product } from '@common/types/product'
+import cn from "classnames";
+import { FC } from "react";
+import s from "./ProductView.module.css";
+import { Container } from "@components/ui";
+import Image from "next/image";
+import { Product } from "@common/types/product";
+import { ProductSlider } from "@components/product";
 
 interface Props {
-  product: Product
+  product: Product;
 }
 
 const ProductView: FC<Props> = ({ product }) => {
-
   return (
     <Container>
-      <div className={cn(s.root, 'fit')}>
-        <div className={cn(s.productDisplay, 'fit')}>
+      <div className={cn(s.root, "fit")}>
+        <div className={cn(s.productDisplay, "fit")}>
           <div className={s.nameBox}>
-            <h1 className={s.name}>Product Name</h1>
+            <h1 className={s.name}>{product.name}</h1>
             <div className={s.price}>
-              50
+              {product.price.value}
               {` `}
-              $
+              {product.price.currencyCode}
             </div>
           </div>
-          <div className={s.imageContainer}>
-            <Image
-              className={s.img}
-              src={"/product-image-placeholder.svg"}
-              alt={'Product Image'}
-              width={1050}
-              height={1050}
-              quality="85"
-            />
-          </div>
+          <ProductSlider>
+            { product.images.map(image =>
+              <div key={image.url} className={s.imageContainer}>
+                <Image
+                  className={s.img}
+                  src={image.url}
+                  alt={image.alt}
+                  width={1050}
+                  height={1050}
+                  quality="85"
+                />
+              </div>
+            )}
+          </ProductSlider>
         </div>
         <div className={s.sidebar}>
           <section>
             <div className="pb-4">
               <h2 className="uppercase font-medium">Color</h2>
-              <div className="flex flex-row py-4">
-                Variant Options Here!
-              </div>
+              <div className="flex flex-row py-4">Variant Options Here!</div>
             </div>
             <div className="pb-14 break-words w-full max-w-xl text-lg">
-              Product description...
+              {product.description}
             </div>
           </section>
           <div>
@@ -58,7 +60,7 @@ const ProductView: FC<Props> = ({ product }) => {
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductView
+export default ProductView;
