5253c4838ebfbcd3641d13c9897c21d8c1553af5
Josh Chappelow <jdchappelow@gmail.com>
2022-12-08 15:54:10 +0900

    feat: color map for swatch selector

--- components/product/Swatch/Swatch.tsx
+++ components/product/Swatch/Swatch.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import s from "./Swatch.module.css";
 import { Check } from "@components/icons";
-import cn from "classnames"
+import cn from "classnames";
+import { isDark, useDark } from "@lib/color"
 
 interface Props {
   color?: string;
   label?: string;
-  active?: boolean
+  active?: boolean;
   variant?: "size" | "color" | string;
   onClick: () => void;
 }
@@ -15,29 +16,27 @@ const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
   label = label?.toLowerCase();
   variant = variant?.toLocaleLowerCase();
 
-  const rootClassName = cn(
-    s.root,
-    {
-      [s.active]: active,
-      [s.color]: color,
-      [s.size]: variant === "size"
-    }
-  )
+  const rootClassName = cn(s.root, {
+    [s.active]: active,
+    [s.color]: color,
+    [s.size]: variant === "size",
+    [s.dark]: color && isDark(color)
+  });
 
   return (
     <button
-      style={color ? {backgroundColor: color} : {}}
+      style={color ? { backgroundColor: color } : {}}
       className={rootClassName}
       {...rest}
     >
-      { variant === "color" && active &&
+      {variant === "color" && active && (
         <span>
           <Check />
         </span>
-      }
-      { variant === "size" ? label : null }
+      )}
+      {variant === "size" ? label : null}
     </button>
-  )
-}
+  );
+};
 
 export default Swatch;
