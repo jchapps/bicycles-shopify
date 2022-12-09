7c13db5c409ffca01c10365ec5f6f638606bdef6
Josh Chappelow <jdchappelow@gmail.com>
2022-12-09 22:02:19 +0900

    refactor: custom hook;

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -4,9 +4,9 @@ import { MutationHook } from "@common/types/hooks";
 export default useAddItem;
 
 export const handler: MutationHook = {
-  fetcher: (input: any) => {
-    console.log("fetching data");
-    return JSON.stringify(input) + "hello function";
+  fetcher: ({fetch, input}) => {
+    const response = fetch(input)
+    return response
   },
   useHook: ({fetch}) => {
     return (input: any) => {
