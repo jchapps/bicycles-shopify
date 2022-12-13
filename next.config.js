21173ab7296b69ee0afc0968fa5672c444cb3b41
Josh Chappelow <jdchappelow@gmail.com>
2022-12-13 16:59:17 +0900

    fix: deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-
 const { withFrameworkConfig } = require("./framework/common/config")
 
 module.exports = withFrameworkConfig({
+
   framework: {
     name: process.env.NEXT_PUBLIC_FRAMEWORK
   },
+
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US"
